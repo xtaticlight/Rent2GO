ef6b8ac68bfb31fef2e1953d4616f488421bb02e
Sui~yans <kimting254@gmail.com>
2016-05-14 17:36:00 +0800

    add model

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,7 +17,6 @@
 							</ul>
 						</div>
 					@endif
-
 					<form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/register') }}">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
---
+++ resources/views/modal_login.blade.php
@@ -0,0 +1,50 @@
+<div id="signIn" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h2 class="modal-title">Login</h2>
+            </div>
+            <div class="modal-body">
+                <div class="container" style="padding-top: 0px;">
+                    <div class="row">
+                        <form role="form" method="POST"
+                              action="{{ url('/auth/login') }}">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <div class="col-md-4 col-lg-3 col-sm-5">
+                                {!! Form::text('email', null, ['class' => 'form-control', 'style' => 'margin-bottom: 6px', 'placeholder' => 'Email']) !!}
+                                {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Password']) !!}
+                                <div class="col-xs-6" style="padding-left: 0px;">
+                                    <div class="label" style="color: gray;">
+                                        <label style="margin-bottom: 7px;">
+                                            {!! Form::checkbox('remember', '1', null,  ['id' => 'keeplogged']) !!}
+                                            Keep me logged in
+                                        </label>
+                                    </div>
+                                    <input type="hidden" name="default_keeplogged" value="0">
+                                </div>
+                                <div class="col-xs-6" style="margin-top: 2px;">
+                                    <a class="label" style="color: gray;"
+                                       href="{{ url('/password/email') }}">Forgot your
+                                        password?</a>
+                                </div>
+                                {!! Form::submit('Log Me Up', ['class' => 'form-control btn-info', 'style' => 'margin-top: 6px']) !!}
+                            </div>
+                        </form>
+                        <div class="col-md-3 hidden-lg col-sm-4">
+                            {!! Form::textarea('body', 'Not yet a member? Sign up for free! Earn Money with school stuff you don\'t use anymore.', ['class' => 'form-control', 'style' => 'height: 94px', 'readonly']) !!}
+                            {!! Form::button('Signup', ['class' => 'form-control btn-info', 'style' => 'margin-top: 6px']) !!}
+                        </div>
+                        <div class="hidden-md col-lg-3 hidden-sm">
+                                            <textarea readonly name="body" id="body" cols="50" rows="50"
+                                                      class="form-control" style="height: 94px">Not yet a member?&#13;&#10;Sign up for free!&#13;&#10;Earn Money with school stuff you don't use anymore.</textarea>
+                            {!! Form::button('Signup', ['class' => 'form-control btn-info', 'style' => 'margin-top: 6px']) !!}
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
