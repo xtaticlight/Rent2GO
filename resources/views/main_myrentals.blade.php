6e988eba7396f77234e888a6b994ec0a51f98b25
Sui~yans <kimting254@gmail.com>
2016-05-15 08:51:23 +0800

    add chabges

---
+++ resources/views/main_myrentals.blade.php
@@ -0,0 +1,122 @@
+<table class="table table-hover col-lg-12 col-md-12 col-xs-12">
+    <thead>
+   <!-- <th>Date</th>--->
+    <th>Name</th>
+    <th>Status</th>
+    <th>Qty</th>
+    <th>Amount</th>
+    </thead>
+    <tbody>
+    @foreach($materials as $data)
+        <tr>
+            <td>{{$data['name']}}</td>
+            <td><a href="#" data-toggle="modal" data-target="#view{{$data['id']}}">{{$data['status']}}</a></td>
+            <td>{{$data['available_qty']}}</td>
+            <td>{{$data['total_due']}}</td>
+         <td><a href = "#" data-toggle ="modal" data-target = "#{{$data['id']}}" ><span class = "glyphicon glyphicon-edit"></span></a></td>
+        </tr>
+        <div class="modal fade modal-info" id="view{{$data['id']}}" role="dialog">
+            <div class="modal-dialog modal-lg">
+
+                <!-- Modal content-->
+                <div class="modal-dialog modal-lg">
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close"
+                                    data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Item Info</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form type="hidden" method="post" action="delete_item"
+                                  id="form1"/>
+                            <input type="hidden" name="_token" value="{{{ csrf_token() }}}"/>
+                            <input type="hidden" name="id" value="{{$data['id']}}"/>
+                            <div class="col-lg-12">
+                                <div class="container col-lg-7">
+                                    <label for="" class="label label-primary"> Pictures</label>
+                                    <div id="infoCarousel" class="carousel slide"
+                                         data-ride="carousel"
+                                         data-interval="1500">
+                                        <ol class="carousel-indicators">
+                                            <li data-target="#infoCarousel" data-slide-to="0"
+                                                class="active"></li>
+                                            <li data-target="#infoCarousel"
+                                                data-slide-to="1"></li>
+                                            <li data-target="#infoCarousel"
+                                                data-slide-to="2"></li>
+                                        </ol>
+                                        <div class="carousel-inner">
+                                            <div class="item active">
+                                                <img src="./assets/img/{{$data['pictures'][0]}}.jpg"
+                                                     width="auto" height="auto;">
+                                            </div>
+                                            @foreach($data['pictures'] as $pictures)
+
+                                                <div class="item">
+                                                    <img src="./assets/img/{{$pictures}}.jpg"
+                                                         width="auto"
+                                                         height="auto;">
+                                                </div>
+                                            @endforeach
+                                        </div>
+
+                                        <a class="left carousel-control" href="#infoCarousel"
+                                           role="button"
+                                           data-slide="prev">
+                                            <span class="glyphicon glyphicon-chevron-left"></span>
+                                        </a>
+
+                                        <a class="right carousel-control" href="#infoCarousel"
+                                           role="button"
+                                           data-slide="next">
+                                            <span class="glyphicon glyphicon-chevron-right"></span>
+                                        </a>
+                                    </div>
+                                    <!--Image Carousel-->
+                                </div>
+                                <div class="container col-lg-5">
+                                    <label for="" class="label label-primary">Descriptions
+                                        :</label>
+                                    <ul>
+                                        <li>{{$data['description']}}</li>
+                                    </ul>
+                                    <label for="" class="label label-primary">Package Qty
+                                        :</label>
+                                    <ul>
+                                        <li>{{$data['available_qty']}}</li>
+                                    </ul>
+                                    <label for="" class="label label-primary">Negotiable Price
+                                        :</label>
+                                    <ul>
+                                        <li>{{$data['total_due']}}</li>
+                                    </ul>
+                                    <label for=""
+                                           class="label label-primary">Availability:</label>
+                                    <ul>
+                                        @if($data['status']=='Available')
+                                            <li>Item On hand</li>
+                                        @else
+                                            <li> Rented By : {{$data['RentBy']}}</li>
+                                        @endif
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="modal-footer">
+                            <a href="./edit_item{{$data['id']}}" class="btn btn-success"><span
+                                        class="glyphicon glyphicon-edit"></span>edit
+                            </a>
+                            <button type="submit" class="btn btn-danger"><span
+                                        class="glyphicon glyphicon-remove"></span>Delete
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
+    @endforeach
+    </tbody>
+</table>
--- resources/views/myrental.blade.php
+++ resources/views/myrental.blade.php
@@ -60,8 +60,6 @@
                         <li><a href="#">others</a></li>
                     </ul>
                 </div>
-
-
                 @if($materials=='')
                     <br><br>
                     <alert class="warning">
@@ -70,121 +68,14 @@
                             rent :D</label>
                     </alert>
                 @else
-                    @foreach($materials as $data)
-                        <div class="col-lg-2" style="align-items: center">
-                            <a href="#" data-toggle="modal" data-target="#{{$data['id']}}"><img
-                                        class="img-responsive" src="./assets/img/{{$data['pictures'][0]}}.jpg"
-                                        style="width: 150px;height:150px; padding-left: 20px"></a>
-                            <label style="margin-left: 80px;">{{$data['name']}}</label>
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            @include('main_myrentals')
                         </div>
-                        <div class="modal fade modal-info" id="{{$data['id']}}" role="dialog">
-                            <div class="modal-dialog modal-lg">
-
-                                <!-- Modal content-->
-                                <div class="modal-dialog modal-lg">
-                                    <!-- Modal content-->
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <button type="button" class="close"
-                                                    data-dismiss="modal">&times;</button>
-                                            <h4 class="modal-title">Item Info</h4>
-                                        </div>
-                                        <div class="modal-body">
-                                            <form type="hidden" method="post" action="delete_item"
-                                                  id="form1"/>
-                                            <input type="hidden" name="_token" value="{{{ csrf_token() }}}"/>
-                                            <input type="hidden" name="id" value="{{$data['id']}}"/>
-                                            <div class="col-lg-12">
-                                                <div class="container col-lg-7">
-                                                    <label for="" class="label label-primary"> Pictures</label>
-                                                    <div id="infoCarousel" class="carousel slide"
-                                                         data-ride="carousel"
-                                                         data-interval="1500">
-                                                        <ol class="carousel-indicators">
-                                                            <li data-target="#infoCarousel" data-slide-to="0"
-                                                                class="active"></li>
-                                                            <li data-target="#infoCarousel"
-                                                                data-slide-to="1"></li>
-                                                            <li data-target="#infoCarousel"
-                                                                data-slide-to="2"></li>
-                                                        </ol>
-                                                        <div class="carousel-inner">
-                                                            <div class="item active">
-                                                                <img src="./assets/img/{{$data['pictures'][0]}}.jpg"
-                                                                     width="auto" height="auto;">
-                                                            </div>
-                                                            @foreach($data['pictures'] as $pictures)
-
-                                                                <div class="item">
-                                                                    <img src="./assets/img/{{$pictures}}.jpg"
-                                                                         width="auto"
-                                                                         height="auto;">
-                                                                </div>
-                                                            @endforeach
-                                                        </div>
-
-                                                        <a class="left carousel-control" href="#infoCarousel"
-                                                           role="button"
-                                                           data-slide="prev">
-                                                            <span class="glyphicon glyphicon-chevron-left"></span>
-                                                        </a>
-
-                                                        <a class="right carousel-control" href="#infoCarousel"
-                                                           role="button"
-                                                           data-slide="next">
-                                                            <span class="glyphicon glyphicon-chevron-right"></span>
-                                                        </a>
-                                                    </div>
-                                                    <!--Image Carousel-->
-                                                </div>
-                                                <div class="container col-lg-5">
-                                                    <label for="" class="label label-primary">Descriptions
-                                                        :</label>
-                                                    <ul>
-                                                        <li>{{$data['description']}}</li>
-                                                    </ul>
-                                                    <label for="" class="label label-primary">Package Qty
-                                                        :</label>
-                                                    <ul>
-                                                        <li>{{$data['available_qty']}}</li>
-                                                    </ul>
-                                                    <label for="" class="label label-primary">Negotiable Price
-                                                        :</label>
-                                                    <ul>
-                                                        <li>{{$data['total_due']}}</li>
-                                                    </ul>
-                                                    <label for=""
-                                                           class="label label-primary">Availability:</label>
-                                                    <ul>
-                                                        @if($data['status']=='Available')
-                                                            <li>Item On hand</li>
-                                                        @else
-                                                            <li> Rented By : {{$data['RentBy']}}</li>
-                                                        @endif
-                                                    </ul>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="modal-footer">
-                                            <a  href ="./edit_item{{$data['id']}}" class="btn btn-success"><span
-                                                        class="glyphicon glyphicon-edit"></span>edit
-                                            </a>
-                                            <button type="submit" class="btn btn-danger"><span
-                                                        class="glyphicon glyphicon-remove"></span>Delete
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                                </form>
-                            </div>
-                        </div>
-                    @endforeach
+                    </div>
                 @endif
             </div>
         </div>
-    </div>
-    </div>
     </div>
     <script>
         document.getElementById("arrow1").innerHTML = "  Rentals Properties";
